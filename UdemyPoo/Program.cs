e71b65f1e0f1486873cedd4d1dcf45f7ff680960
AnndreJunior <anndre.junior15@gmail.com>
2024-04-16 10:52:08 -0300

    notes matrizes

--- UdemyPoo/Program.cs
+++ UdemyPoo/Program.cs
@@ -4,43 +4,17 @@
     {
         Console.Clear();
 
-        var lista = new List<string>
-        {
-            "sla",
-            "sim"
-        };
+        double[,] matriz = new double[2, 3];
 
-        var users = new List<User>
-        {
-            new("André", 17),
-            new("Rian", 15)
-        };
+        Console.WriteLine(matriz.Length);
 
-        var algo = lista.FindAll(x => x[0] == 's');
+        // quantidade de linhas
+        Console.WriteLine(matriz.Rank);
 
-        Console.WriteLine(algo[0]);
+        // tamanho da primeira dimensão (linhas)
+        Console.WriteLine(matriz.GetLength(0));
 
-        foreach (var i in lista)
-            Console.WriteLine(i);
-
-        var andre = users.Find(user => user.Name == "André");
-
-        Console.WriteLine(andre?.Name);
-        Console.WriteLine(andre?.Age);
-
-        foreach (var user in users)
-            Console.WriteLine($"Nome: {user.Name}, idade: {user.Age}");
-    }
-}
-
-class User
-{
-    public string Name { get; private set; }
-    public int Age { get; private set; }
-
-    public User(string name, int age)
-    {
-        Name = name;
-        Age = age; ;
+        // tamanho da segunda dimensão (colunas)
+        Console.WriteLine(matriz.GetLength(1));
     }
 }
